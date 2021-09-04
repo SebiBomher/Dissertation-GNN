final fix for LR, added normalization

Scripts/data_proccess.py
@@ -6,9 +6,7 @@ from typing import Union
 from geopy.distance import geodesic
 from glob import glob
 from enum import Enum
-
-from torch_geometric.data.data import Data
-
+from sklearn.preprocessing import normalize
 class DatasetSizeNumber(Enum):
     r"""
         Number of DatasetSizes, the total number of nodes per each dataset type.
@@ -150,7 +148,6 @@ class DataReader():
                 nodes_index = 0
         return new_X,new_Y
 
-
     def __read_data(self) -> None :
         r"""
             Instance function.
@@ -173,7 +170,7 @@ class DataReader():
                         Y.append((float)(line[11]))
                         X.append([(float)(line[9]),(float)(line[10])])
             nb_days += 1
-        self.X = X
+        self.X = np.transpose([normalize(row) for row in np.transpose(X)])
         self.Y = Y
         self.nb_days = nb_days
 

Scripts/datasetsClasses.py
@@ -79,6 +79,7 @@ class LinearRegressionDataset():
 
         
     def __save_dataset(self):
+        if not LinearRegressionDataset.need_load(self.proccessed_data_path) : return
         X,Y = self.datareader.get_clean_data_by_nodes(DatasetSize.Medium,self.proccessed_data_path)
 
         X = self.__arrange_data(X,DatasetSizeNumber.Medium.value)
@@ -110,12 +111,12 @@ class LinearRegressionDataset():
         name_x = os.path.join(self.proccessed_data_path_model,"Data",'X_{0}*.npy'.format(str(time_index))) 
         name_y = os.path.join(self.proccessed_data_path_model,"Data",'Y_{0}*.npy'.format(str(time_index)))
         for filename in glob.glob(name_x):
+            node_id = filename[-10:-4]
             X = np.load(filename)
         for filename in glob.glob(name_y):
             Y = np.load(filename)
             
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2,shuffle = False)
-        node_id = name_x[-6:]
         return  X_train, X_test, Y_train, Y_test, node_id
 
     def __next__(self):